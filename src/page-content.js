6f02d2918cadbd4f6fb7c1034469f5ce0fa338c6
Carson-Key <keyc@oregonstate.edu>
2019-11-23 14:43:34 -0800

    Update page-content with refactored code

--- src/page-content.js
+++ src/page-content.js
@@ -2,16 +2,17 @@ import { addElement } from './utilities/addElement.js'
 import { websiteContent } from './content.js'
 import { isActive } from './utilities/isActive.js'
 
-$(document).ready( function() {
-  for (const page in body.elements) {
-    var active = ""
-    if (page == 0) {
-      active = " is-active"
-    }
-    $("#body").append(" \
-      <section class=\"mdl-layout__tab-panel" + active + "\" id=\"" + body.elements[page].id + "\"> \
-        <div class=\"page-content\"><p>THINGS " + page + "</p></div> \
-      </section> \
-    ")
-  }
-})
+function createPage(pageNumber, active) {
+  var pageID = websiteContent.elements[pageNumber].id
+  var page
+
+  page = "<section class=\"mdl-layout__tab-panel" + active + "\" id=\"" + pageID + "\">"
+  page += "<div class=\"page-content\"><p>THINGS " + pageNumber + "</p></div>"
+  page += "</section>"
+
+  return page
+}
+
+for (const pageNumber in websiteContent.elements) {
+  addElement(createPage(pageNumber, isActive(pageNumber, 0)), "#body")
+}
