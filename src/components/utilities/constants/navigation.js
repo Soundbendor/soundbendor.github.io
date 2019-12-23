47c78aec5fc809e1e398b017ab97778166a7652d
Carson-Key <keyc@oregonstate.edu>
2019-12-22 17:01:35 -0800

    Add constants

---
+++ src/components/utilities/constants/navigation.js
@@ -0,0 +1,9 @@
+export const NAVOUTLINE = "\
+  <div class=\"mdc-tab-bar\" role=\"tablist\">\
+    <div class=\"mdc-tab-scroller\">\
+      <div class=\"mdc-tab-scroller__scroll-area\">\
+        <div class=\"mdc-tab-scroller__scroll-content\"></div>\
+      </div>\
+    </div>\
+  </div>\
+"
