ac3ecfdccaded44341e46d828ad3604007b43293
Carson-Key <keyc@oregonstate.edu>
2019-12-22 17:08:21 -0800

    Remove dead code

--- src/components/utilities/addElement.js
+++ src/components/utilities/addElement.js
@@ -1,8 +1,3 @@
 export function addElement(element, parent) {
   $(parent).append(element)
-  // upgrade($(parent).get()[0])
-}
-
-function upgrade(parent) {
-  componentHandler.upgradeElement($(parent).get()[0])
 }
