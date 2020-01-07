50ba1d42e408c60d42e66c30a9f948c5864bc058
Carson-Key <keyc@oregonstate.edu>
2020-01-07 14:31:38 -0800

    Remove old imports

--- src/components/containers/navigation/index.js
+++ src/components/containers/navigation/index.js
@@ -1,6 +1,4 @@
 import { initializeNav, initializeNavElements } from './navigation.js'
-// import { addElement } from './utilities/addElement.js'
-// import { isActive } from './utilities/isActive.js'
 
 export function generateNavigation(elements, landingPageIndex) {
   initializeNav()
