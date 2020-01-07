ea27f1d49b057154a37d38a85ef01166c6c886d1
Carson-Key <keyc@oregonstate.edu>
2020-01-06 16:54:19 -0800

    Variablize websiteContent

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import { generatePageContent } from './components/containers/page-content/index.
 import { initializeScripts } from './components/containers/scripts/index.js'
 
 export function generateWebsite(websiteContent) {
-  generateNavigation(websiteContent.navigation, websiteContent.landingPageIndex)
-  generatePageContent(websiteContent.pages, websiteContent.landingPageIndex)
+  const navigation = websiteContent.navigation
+  const pages = websiteContent.pages
+  const landingPageIndex = websiteContent.landingPageIndex
+
+  generateNavigation(navigation, landingPageIndex)
+  generatePageContent(pages, landingPageIndex)
   initializeScripts()
 }
