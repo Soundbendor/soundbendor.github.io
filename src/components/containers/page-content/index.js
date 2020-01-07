3dd3896b1757ab2ac19550c3e24176e8368eb6d8
Carson-Key <keyc@oregonstate.edu>
2020-01-06 18:11:20 -0800

    Move generatePageContent body into functions

--- src/components/containers/page-content/index.js
+++ src/components/containers/page-content/index.js
@@ -1,30 +1,6 @@
-import { addElement } from '../../utilities/addElement.js'
-import { isActivePage as isActive } from '../../utilities/isActive.js'
-import { createPage, updatePage } from './elements.js'
+import { initializePages, initializePageChange } from './page-content.js'
 
 export function generatePageContent(pages, landingPageIndex) {
-  var pageIds = []
-
-  for (const pageNumber in pages) {
-    const pagePath = pages[pageNumber]
-    const pageId = "page-mdc-tab-" + (parseInt(pageNumber) + 1)
-    pageIds.push(pageId)
-
-    createPage(pagePath, isActive(pageNumber, landingPageIndex), pageId)
-  }
-
-  pageChange(pageIds)
-}
-
-function pageChange(pageIds) {
-  const nav = mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.mdc-tab-bar'))
-
-  nav.listen('MDCTab:interacted', function (event) {
-    const pageId = event.detail.tabId
-    const pageIndex = parseInt(pageId.split("-")[2])
-
-    for (const idNumber in pageIds) {
-      updatePage(isActive(parseInt(idNumber) + 1, pageIndex), pageIds[idNumber])
-    }
-  })
+  const pageIds = initializePages(pages, landingPageIndex)
+  initializePageChange(pageIds)
 }
---
+++ src/components/containers/page-content/page-content.js
@@ -0,0 +1,30 @@
+import { addElement } from '../../utilities/addElement.js'
+import { isActivePage as isActive } from '../../utilities/isActive.js'
+import { createPage, updatePage } from './elements.js'
+
+export function initializePages(pages, landingPageIndex) {
+  var pageIds = []
+
+  for (const pageNumber in pages) {
+    const pagePath = pages[pageNumber]
+    const pageId = "page-mdc-tab-" + (parseInt(pageNumber) + 1)
+    pageIds.push(pageId)
+
+    createPage(pagePath, isActive(pageNumber, landingPageIndex), pageId)
+  }
+
+  return pageIds
+}
+
+export function initializePageChange(pageIds) {
+  const nav = mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.mdc-tab-bar'))
+
+  nav.listen('MDCTab:interacted', function (event) {
+    const pageId = event.detail.tabId
+    const pageIndex = parseInt(pageId.split("-")[2])
+
+    for (const idNumber in pageIds) {
+      updatePage(isActive(parseInt(idNumber) + 1, pageIndex), pageIds[idNumber])
+    }
+  })
+}
