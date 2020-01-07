7f6766533abef6d8476631c35758e3d3816ef00e
Carson-Key <keyc@oregonstate.edu>
2020-01-07 14:49:02 -0800

    Add some documentation for the content object

--- src/content.js
+++ src/content.js
@@ -1,3 +1,8 @@
+// Full object at bottom of doc with documentation
+//             |
+//             |
+//             V
+
 export const websiteContent = {
   websiteTitle: "SoundBendor",
   landingPageIndex: 0,
@@ -8,3 +13,16 @@ export const websiteContent = {
     "./src/pages/thirdPage/index.html"
   ]
 }
+
+// Everything the content object can have
+// {
+//  websiteTitle: "Some string, should be short but descriptive for SEO"
+//  landingPageIdex: An index (number) within the navigation, this used to determine what page is first seen
+//  navigation: ["An array of strings", "Used to generate text on the tabs"]
+//  pages: [
+//    "An array of strings that represent paths to the page",
+//    "Must match up with navigation array",
+//    "Index 0 in navigation will go to index 0 of pages",
+//    "the path must look like "./src/pages/{page file}/{index html}""
+//  ]
+// }
